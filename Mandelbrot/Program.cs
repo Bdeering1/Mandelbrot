bd6e973f8b11bf08151f1489994c1c255764969a
Bryn Deering <bdeering@uoguelph.ca>
2022-06-12 17:53:37 -0400

    Changed launch settings

--- Mandelbrot/Program.cs
+++ Mandelbrot/Program.cs
@@ -4,12 +4,14 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
+        //var builder = WebApplication.CreateBuilder(args);
+        //var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        //app.MapGet("/", () => "Hello World!");
 
-        app.Run();
+        //app.Run();
+
+        Console.WriteLine("Hello world!");
     }
 }
 
