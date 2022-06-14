50c87ea701dc7880be45ba4a12b2ec7cbd167564
JukeBro <jidjosh2006@gmail.com>
2022-06-13 21:12:14 -0400

    add complex numbers with addition and multiplication

---
+++ Mandelbrot/Value Objects/Complex.cs
@@ -0,0 +1,26 @@
+﻿namespace Value_Objects
+{
+    public struct Complex
+    {
+        public Complex(decimal real, decimal imaginary)
+        {
+            r = real;
+            i = imaginary;
+        }
+
+        public decimal r;
+        public decimal i;
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+            a.r += b.r;
+            a.i += b.i;
+            return a;
+        }
+
+        public static Complex operator *(Complex a, Complex b)
+        {
+            return new Complex(a.r * b.r - a.i * b.i, a.r * b.i + a.i * b.r);
+        }
+   
+}
