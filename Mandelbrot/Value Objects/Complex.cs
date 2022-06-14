f85ee47e0483acda65e72f063925e13cf359381e
JukeBro <jidjosh2006@gmail.com>
2022-06-14 19:03:45 -0400

    add subtract operator

--- Mandelbrot/Value Objects/Complex.cs
+++ Mandelbrot/Value Objects/Complex.cs
@@ -18,6 +18,13 @@
             return a;
         }
 
+        public static Complex operator -(Complex a, Complex b)
+        {
+            a.r -= b.r;
+            a.i -= b.i;
+            return a;
+        }
+
         public static Complex operator *(Complex a, Complex b)
         {
             return new Complex(a.r * b.r - a.i * b.i, a.r * b.i + a.i * b.r);
