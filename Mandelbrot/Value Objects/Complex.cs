da96bee2e358dc1406333ce88a6d212f7f92c575
Bryn Deering <bdeering@uoguelph.ca>
2022-06-14 01:35:01 -0400

    Added ToString method to complex struct + minor refactoring

--- Mandelbrot/Value Objects/Complex.cs
+++ Mandelbrot/Value Objects/Complex.cs
@@ -11,16 +11,14 @@
             i = imaginary;
         }
 
-        public static Complex operator +(Complex a, Complex b)
-        {
-            a.r += b.r;
-            a.i += b.i;
-            return a;
-        }
+        public static Complex operator +(Complex a, Complex b) =>
+            new Complex(a.r + b.r, a.i + b.i);
 
-        public static Complex operator *(Complex a, Complex b)
-        {
-            return new Complex(a.r * b.r - a.i * b.i, a.r * b.i + a.i * b.r);
-        }
+        public static Complex operator *(Complex a, Complex b) =>
+            new Complex(a.r * b.r - a.i * b.i,
+                        a.r * b.i + a.i * b.r);
+
+
+        public override string ToString() => $"{r} + {i}i";
     }
 }
