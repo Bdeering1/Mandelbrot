b887e4a6f7887af48422c1241a52ccc5912772c2
JukeBro <jidjosh2006@gmail.com>
2022-06-14 21:05:46 -0400

    Add preliminary escape time calculation

---
+++ Mandelbrot/EscapeTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Value_Objects;
+
+namespace Mandelbrot
+{
+    public class EscapeTime
+    {
+        public static int CalcEscapeTime(Complex pt)
+        {
+            var constant = new Complex(pt.r, pt.i);
+            var current = new Complex(0, 0);
+
+            int iter = 0;
+            int maxIterations = 200;
+
+            while(Math.Sqrt((Math.Pow(current.r, 2) + Math.Pow(current.i, 2))) <= 2 && iter <= maxIterations)
+            {
+                current = current * current + constant;
+                iter++;
+            }
+            return iter;
+        }
+    }
+}
