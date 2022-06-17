95bf2502cb3ce4fd72eb0c22a3259a8269a42202
JukeBro <jidjosh2006@gmail.com>
2022-06-16 21:37:10 -0400

    Added color gradient generation

---
+++ Mandelbrot/Core/GenerateColors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Mandelbrot.Core
+{
+    public class GenerateColors
+    {
+        public static List<Color> GetGradients(Color start, Color end, int rangeMax)
+        {
+            //set max & min RGB values to interpolate between
+            int rMax = start.R;
+            int rMin = end.R;
+            int gMax = start.G;
+            int gMin = end.G;
+            int bMax = start.B;
+            int bMin = end.B;
+
+            var colors = new List<Color>();
+
+            //do the thing (interpolate RGB values then add them to the list)
+            for (int i = 0; i < rangeMax; i++)
+            {
+                var rAverage = rMin + (rMax - rMin) * i / rangeMax;
+                var gAverage = gMin + (gMax - gMin) * i / rangeMax;
+                var bAverage = bMin + (bMax - bMin) * i / rangeMax;
+                colors.Add(Color.FromArgb(rAverage, gAverage, bAverage));
+            }
+            return colors;
+        }
+    }
+}
