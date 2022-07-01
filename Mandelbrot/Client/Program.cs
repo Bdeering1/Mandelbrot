673442ddaa38eb9cc88847575e096e793708ab06
Bryn Deering <bdeering@uoguelph.ca>
2022-06-30 22:17:54 -0400

    Created new Blazor WASM app from template and moved existing code to new project

--- Mandelbrot/Client/Program.cs
+++ Mandelbrot/Client/Program.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
-using System.Net.Http;
-using System;
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Mandelbrot.Client;
 
-namespace Client;
+namespace Mandelbrot.Client;
 
 public class Program
 {
@@ -12,7 +10,7 @@ public class Program
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
-        //builder.RootComponents.Add<HeadOutlet>("head::after");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
