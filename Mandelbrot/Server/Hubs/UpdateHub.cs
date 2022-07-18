fff24313b48aa14b11d9276b34d111aee1a72b18
Josh <jidjosh2006@icloud.com>
2022-07-17 21:23:15 -0400

    now logs time taken to execute

--- Mandelbrot/Server/Hubs/UpdateHub.cs
+++ Mandelbrot/Server/Hubs/UpdateHub.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Diagnostics;
+using System.Drawing;
 using Mandelbrot.Core;
 using Mandelbrot.Server.Core;
 using Microsoft.AspNetCore.SignalR;
@@ -12,7 +13,12 @@ namespace Mandelbrot.Server.Hubs
         {
             Console.WriteLine("Request received");
             List<Color> colors = ColorGenerator.GetGradients(200);
+
+            Stopwatch s = new Stopwatch();
+            s.Start();
             var set = Convert.ToBase64String(GenerateSet.GetBitmap(colors, 500, 500).Encode(SKEncodedImageFormat.Png, 100).ToArray());
+            s.Stop();
+            Console.WriteLine(s.ElapsedMilliseconds);
 
             await Clients.All.SendAsync("ReceiveBitmap", set);
             Console.WriteLine("Bitmap sent");
